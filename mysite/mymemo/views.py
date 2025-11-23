ee0bf32d15d9f6e6adb6f83cff74b6615c363173
mocaron <mocaron00@ewhain.net>
2025-11-24 00:18:08 +0900

    urls

--- mysite/mymemo/views.py
+++ mysite/mymemo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("서디콘 하잉");
+
+def create(request):
+    content = request.GET['memoContent']
+    return HttpResponse("사용자 입력 = "+content)
