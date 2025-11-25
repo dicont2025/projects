dd12cfa8071658e6a3caffc727d88998947625dd
dicont2025 <mocaron00@sonline20.sen.go.kr>
2025-11-25 09:07:01 +0900

    Update settings.py

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mymemo.apps.MymemoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
