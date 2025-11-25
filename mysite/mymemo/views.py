8eda066f3f29ece108ed62f6c66208a240cfd314
dicont2025 <mocaron00@sonline20.sen.go.kr>
2025-11-25 09:09:31 +0900

    Update views.py

--- mysite/mymemo/views.py
+++ mysite/mymemo/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("서디콘 하잉");
+   return render(request, "main.html")
 
 def create(request):
-    content = request.GET['memoContent']
+    content = request.POST['memoContent']
     return HttpResponse("사용자 입력 = "+content)
