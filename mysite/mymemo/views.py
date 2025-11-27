c17c41877410f956ddd01ca1db2e7f77f3534f85
dicont2025 <mocaron00@sonline20.sen.go.kr>
2025-11-27 10:05:23 +0900

    Update views.py

--- mysite/mymemo/views.py
+++ mysite/mymemo/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-   return render(request, "main.html")
-
+    lists = Memo.objects.all()
+    data = {'lists':lists}
+    return render(request, "main.html", data)
+   
 def create(request):
     content = request.POST['memoContent']
+    usermemo = Memo(text = content)
+    usermemo.save()
     return HttpResponse("사용자 입력 = "+content)
