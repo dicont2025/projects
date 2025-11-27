d2a54c85a8265b87991fa332587fc94abdb3871c
dicont2025 <mocaron00@sonline20.sen.go.kr>
2025-11-27 10:03:54 +0900

    Update models.py

--- mysite/mymemo/models.py
+++ mysite/mymemo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Memo(models.Model):
+    text = models.CharField(max_length=200)
+    published_date = models.DateTimeField(auto_now_add=True)
