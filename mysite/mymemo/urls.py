595cfd93f8547d9c823234927ee3f4d7f2a1f65d
dicont2025 <mocaron00@sonline20.sen.go.kr>
2025-12-02 11:45:18 +0900

    Update urls.py

--- mysite/mymemo/urls.py
+++ mysite/mymemo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('create/', views.create),
+    path('', views.index, name="index"),
+    path('create/', views.create, name="create"),
+    path('edit/<str:idx>', views.editPage, name="edit"),
 ]
